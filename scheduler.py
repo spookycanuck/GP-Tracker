ea675e95617a6a4ffa6de080d95c5be0e3d623af
EM <eric.mann2393@gmail.com>
2022-05-01 17:12:31 -0400

    initial commit of working prototype for creating tweets on timer

---
+++ scheduler.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+import tweepy, time, sys
+import keys as k
+
+argfile = str(sys.argv[1])
+
+API_KEY = k.apiKey
+API_SECRET = k.apiSecret
+CONSUMER_TOKEN = k.accessToken
+CONSUMER_SECRET = k.accessSecret
+
+auth = tweepy.OAuthHandler(API_KEY, API_SECRET)
+auth.set_access_token(CONSUMER_TOKEN, CONSUMER_SECRET)
+api = tweepy.API(auth)
+
+filename = open(argfile,'r')
+f = filename.readlines()
+filename.close()
+
+for line in f:
+    api.update_status(line)
+    print('tweet number', f.index(line)+1, 'posted')
+    time.sleep(65)
