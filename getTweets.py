644cedfbb6209a5deee69b8fe91b33a0ead87535
EM <eric.mann2393@gmail.com>
2022-05-05 13:43:03 -0400

    add some data handling to script

--- getTweets.py
+++ getTweets.py
@@ -15,19 +15,35 @@ auth.set_access_token(ACCESS_TOKEN, ACCESS_SECRET)
 api = tweepy.API(auth)
 
 def batch_delete(api):
-    print("Get all tweets from the account @%s?" % api.verify_credentials().screen_name)
+    x=[]
+    print("Get tweets from the account @%s?" % api.verify_credentials().screen_name)
     print("Type yes to carry out this action.")
-    do_delete = input("> ")
-    if do_delete.lower() == 'y' or do_delete.lower() == 'yes':
+    get_tweets = input(">> ")
+    if get_tweets.lower() == 'y' or get_tweets.lower() == 'yes':
         for status in tweepy.Cursor(api.user_timeline).items():
-            try:
-                api.get_status(status.id)
-                print("Tweet ID:", status.id)
-                sys.stdout.flush()
-            except Exception:
-                traceback.print_exc()
-                print("Failed to retrieve ID:", status.id)
-                sys.stdout.flush()
+            x.append(status)
+        if (len(x)<50):
+            for status in tweepy.Cursor(api.user_timeline).items():
+                try:
+                    api.get_status(status.id)
+                    print("Tweet ID:", status.id)
+                    sys.stdout.flush()
+                except Exception:
+                    traceback.print_exc()
+                    print("Failed to retrieve ID:", status.id)
+                    sys.stdout.flush()
+        else:
+            print("\nERROR: More than 50 tweets detected!\nPrinting the last 15:")
+            for status in x[-15:]:
+                try:
+                    api.get_status(status.id)
+                    print("Tweet ID:", status.id)
+                    sys.stdout.flush()
+                except Exception:
+                    traceback.print_exc()
+                    print("Failed to retrieve ID:", status.id)
+                    sys.stdout.flush()
+
  
 if __name__ == "__main__":
     # api = oauth_login(ACCESS_TOKEN, ACCESS_SECRET)
