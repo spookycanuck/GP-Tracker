baa6b88ad14d798c34f7b12ca843359fb42f6a4a
EM <eric.mann2393@gmail.com>
2022-11-11 10:33:32 -0500

    refactor timeGetter

--- timeGetter.py
+++ timeGetter.py
@@ -2,11 +2,14 @@ from datetime import datetime, date
 from dateutil import parser
 import csv
 
+FILENAME = 'files/scheduleX.csv'
+INDEX = 0
 
 headers = []
 race_list = []
+output_X = []
 
-with open('files/schedule4.csv', mode='r') as csv_file:
+with open(FILENAME, mode='r') as csv_file:
     csv_reader = csv.reader(csv_file)
     line_count = 0
     for row in csv_reader:
@@ -40,27 +43,63 @@ with open('files/schedule4.csv', mode='r') as csv_file:
 #     print("Match!")
 # else:
 #     print("no match :(")
+
+
+def normalizeTime():
 # ========================
-
-
 # Matches time string
 # ========================
-now = datetime.now()
-race = race_list[5]['time']
-print("\nRace time from CSV: ", race)
-print("Current time:", now)
+    now = datetime.now()
+    race = race_list[INDEX]['time']
+    print("\nRace time from CSV: ", race)
+    print("Current time:", now)
 
-# raceTimeObj = datetime.strptime(race_list[4]['time'],'%b %d - %I:%M %p')
-raceTimeObj = parser.parse(race)
-current_time = now.replace(microsecond=0)
+    # raceTimeObj = datetime.strptime(race_list[4]['time'],'%b %d - %I:%M %p')
+    raceTimeObj = parser.parse(race)
+    current_time = now.replace(microsecond=0)
 
-print('\n-----------------')
-print("Race time as local dt Obj: ", raceTimeObj)
-print("Current time as clean dt Obj: ", current_time)
+    print('\n-----------------')
+    print("Race time as local dt Obj: ", raceTimeObj)
+    print("Current time as clean dt Obj: ", current_time)
 
-time_diff = raceTimeObj - current_time
-td_sec = time_diff.total_seconds()
-td_hr = td_sec/(60 * 60)
-print('\n-----------------')
-print('time diff: ', time_diff)
-print('total hrs: ', round(td_hr, 2))
+    time_diff = raceTimeObj - current_time
+    td_sec = time_diff.total_seconds()
+    td_hr = td_sec/(60 * 60)
+    print('\n-----------------')
+    print('time diff: ', time_diff)
+    print('total hrs: ', round(td_hr, 2), '\n')
+
+def makeNewList():
+    now = datetime.now()
+    for i, x in enumerate(race_list):
+        # print(x['time'])
+        if (x['time'] == "Race Completed"):
+            print(x['time'])
+        elif (x['time'] == "Race has been CANCELED"):
+            print(x['time'])
+        else:
+            raceTimeX = parser.parse(x['time'])
+            # print(raceTimeX)
+            current = now.replace(microsecond=0)
+            time_d = raceTimeX - current
+            td_sec = time_d.total_seconds()
+            td_hr = td_sec/(60 * 60)
+            if (td_hr < 0):
+                print('Race Completed_X')
+            else:
+                print(x['date'])
+                race_X = {
+                    "date": x['date'],
+                    "race": x['race'],
+                    "location": x['location'],
+                    "time": x['time'],
+                    "watch": x['watch']
+                }
+                output_X.append(race_X)
+                with open('files/scheduleX.csv', 'w') as csvfile: 
+                    wr = csv.DictWriter(csvfile, fieldnames=headers) 
+                    wr.writeheader()
+                    wr.writerows(output_X)
+
+normalizeTime()
+# makeNewList()
